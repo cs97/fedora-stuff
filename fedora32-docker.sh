99c59e5fd744700d35948e3238f7df3241bb7052
l3f7s1d3 <l3f7s1d3@protonmail.com>
2020-08-12 11:09:37 +0200

    Create fedora32-docker.sh

---
+++ fedora32-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo dnf remove docker-*
+sudo dnf config-manager --disable docker-*
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+sudo firewall-cmd --permanent --zone=trusted --add-interface=docker0
+sudo firewall-cmd --permanent --zone=FedoraWorkstation --add-masquerade
+sudo dnf install moby-engine docker-compose
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl reboot
