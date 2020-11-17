0e98ba9ce5b5aa2a8d8522fc8683ec88a7c01cd1
leftside97 <l3f7s1d3@protonmail.com>
2020-11-17 16:46:26 +0100

    Update fedora-i3.sh

--- fedora-i3.sh
+++ fedora-i3.sh
@@ -2,6 +2,11 @@
 
 USER='user'
 
-dnf install xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drv-libinput xorg-x11-drv-evdev xrandr i3 i3lock feh
+dnf install xorg-x11-server-Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drv-libinput xorg-x11-drv-evdev xrandr i3 i3lock feh pulseaudio alsa-utils
 useradd -m -G users,wheel,audio -s /bin/bash $USER
 echo "exec i3" >> /home/$USER/.xinitrc
+
+# xrandr --output Vritual-1 --mode 1920x1440
+# amixer sset Master 100% unmute
+
+
